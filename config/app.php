1ac886161137ebf714053a98f10e438e1c84e663
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-01-16 14:23:31 +0545

    updated settings page; bug fix

--- config/app.php
+++ config/app.php
@@ -229,6 +229,14 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // Libraries
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Shortcode' => Webwizo\Shortcodes\Facades\Shortcode::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Avatar' => Laravolt\Avatar\Facade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Site' => \Robust\Core\Helpage\Site::class,
     ],
 
 ];
