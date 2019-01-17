e99823e33dd73b5899f4285ba77d96d0beb8cc52
Manisha <maansi.chaudhary3@gmail.com>
2019-01-17 14:55:21 +0545

    preview-fix

--- packages/robust/dynamic-forms/resources/views/admin/forms/preview.blade.php
+++ packages/robust/dynamic-forms/resources/views/admin/forms/preview.blade.php
@@ -2,7 +2,7 @@
 @section('custom_design')
     <div class="container preview-page">
         @include("core::admin.partials.messages.info")
-        <div class="col-md-10 preview--container">
+        <div class="col-md-12 preview--block">
             <div id='preview--container__form'></div>
         </div>
     </div>
