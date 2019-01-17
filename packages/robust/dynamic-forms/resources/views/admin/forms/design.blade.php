c894a11171ace3802536cfba90096e9d0464da2a
Manisha <maansi.chaudhary3@gmail.com>
2019-01-17 14:41:31 +0545

    design-form-fix

--- packages/robust/dynamic-forms/resources/views/admin/forms/design.blade.php
+++ packages/robust/dynamic-forms/resources/views/admin/forms/design.blade.php
@@ -1,9 +1,11 @@
 @extends('core::admin.layouts.sub-layouts.blank')
 @section('custom_design')
     @inject('form_helper', 'Robust\DynamicForms\Helpers\FormHelper')
-    <div class="dynamic-form">
-        {{ Form::select('display', ['form' => 'form', 'wizard' => 'wizard'], null, ['class' => 'dynamic--form__type']) }}
-        <a href="javascript:void(0)" class = "dynamic-form__save"> Save </a>
-        <div id='designer'></div>
+    <div>
+        <div class="design--form--header">
+            {{ Form::select('display', ['form' => 'form', 'wizard' => 'wizard'], null, ['class' => 'dynamic--form__type']) }}
+            <a href="javascript:void(0)" class = "dynamic-form__save btn-theme pull-right"> Save </a>
+        </div>
+        <div id='designer' class="design--form--block"></div>
     </div>
 @endsection
