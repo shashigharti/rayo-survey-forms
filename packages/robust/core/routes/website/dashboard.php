327fa2b31f7adaf8f06e82fbf71889bf8dd47045
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-01-18 10:07:29 +0545

    added profile for frontend user

--- packages/robust/core/routes/website/dashboard.php
+++ packages/robust/core/routes/website/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['prefix' => 'user', 'as' => 'website.user.', 'group' => 'User Dashboard'], function () {
+Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'user', 'as' => 'website.user.', 'group' => 'User Dashboard'], function () {
     Route::resource('dashboards', '\Robust\Core\Controllers\Website\DashboardController');
 });
 
