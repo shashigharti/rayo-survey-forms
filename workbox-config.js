4cf29d743b771d160c08117362de47eab7b705b3
Zenish Shrestha <zenishshr@gmail.com>
2019-01-18 16:38:48 +0545

    PWA Init - SW Register on public.

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "globDirectory": "public/",
+    "globPatterns": [
+        "**/*.css",
+        "**/*.html",
+        "**/*.png",
+        "**/*.js",
+    ],
+    "swDest": "public/sw.js",
+    "swSrc": "resources/js/sw.js"
+};
