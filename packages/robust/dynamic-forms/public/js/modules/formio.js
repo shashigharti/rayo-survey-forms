90e2aedb5d7cbce083448138c256852ff0f57502
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-01-23 15:06:57 +0545

    updated js

--- packages/robust/dynamic-forms/public/js/modules/formio.js
+++ packages/robust/dynamic-forms/public/js/modules/formio.js
@@ -620,5 +620,12 @@
                 readOnly: true
             });
         }
+
+        if ($('#form__show').length > 0) {
+            let formComponents = $('#form__show').data('form-components');
+            Formio.createForm(document.getElementById('form__show'), formComponents, {
+                readOnly: true
+            });
+        }
     });
 }(jQuery, FRW, window, document));
