43d955dabc73cf7997474ec14bc5e3ca948e7d00
Zenish Shrestha <zenishshr@gmail.com>
2019-01-25 15:32:21 +0545

    Added forms on left menu bar for testing purposes

--- packages/robust/core/resources/views/admin/partials/menus/left-menu.blade.php
+++ packages/robust/core/resources/views/admin/partials/menus/left-menu.blade.php
@@ -7,7 +7,7 @@
                 @set('sub_menus', $menu_helper->getSubMenus($menu->id))
                 @can($menu->permission)
                 <div class="item-tooltip">
-                    <li class="item">                    
+                    <li class="item">
                         <a href="javascript:void(0)"><i class="icon {{ $menu->icon }}" aria-hidden="true"></i></a>
                         <span class="btn-class">
                         <a class="menu_item"
@@ -29,6 +29,20 @@
                 </div>
                 @endcan
             @endif
+        @endforeach
+
+        // Test purpose only
+
+        @foreach($menu_helper->getAllForms() as $form)
+            <div class="item-tooltip">
+                <li class="item">
+                    <a href="javascript:void(0)"><i class="icon md-assignment-o" aria-hidden="true"></i></a>
+                    <span class="btn-class">
+                        <a class="menu_item" href="{{route('admin.userform', $form['slug'])}}">{{$form['title']}}</a>
+                    </span>
+                </li>
+                <span class="tooltiptext tooltip-right">{{$form['title']}}</span>
+            </div>
         @endforeach
     </ul>
 </div>
--- packages/robust/core/src/Helpers/MenuHelper.php
+++ packages/robust/core/src/Helpers/MenuHelper.php
@@ -2,6 +2,7 @@
 namespace Robust\Core\Helpers;
 
 use Robust\Core\Models\Menu;
+use Robust\DynamicForms\Models\Form;
 
 /**
  * Class MenuHelper
@@ -66,6 +67,16 @@ class MenuHelper
         //children.children is used to retrieve children of childrens
         $menus = Menu::where('parent_id', 0)->where('package_name', '=', $package_name)->with('children', 'children.children')->get();
         return isset($menus) ? $menus : [];
+    }
+
+    /**
+     * Test purpose only
+     * @param \Robust\DynamicForms\Models\Form $form
+     * @return mixed
+     */
+    public function getAllForms()
+    {
+        return Form::select('title', 'slug')->get();
     }
 
 
