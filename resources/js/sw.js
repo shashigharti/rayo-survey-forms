88926eaf384c3568575037c22fbbab881755b726
Zenish Shrestha <zenishshr@gmail.com>
2019-01-30 10:21:18 +0545

    SW updated

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -5,6 +5,7 @@ if (workbox) {
 
     // Debug mode set to true
     workbox.setConfig({ debug: true });
+    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
     workbox.precaching.precacheAndRoute([]);
     workbox.routing.registerRoute(
@@ -17,12 +18,59 @@ if (workbox) {
         workbox.strategies.networkFirst()
     );
 
+    // workbox.routing.registerRoute(
+    //     new RegExp('/admin/user/form/.*'),
+    //     function() {
+    //         return caches.match('/assets/website/html/layout.html');
+    //     }
+    // );
+
+    // const handlerCb = ({url, event, params}) => {
+    //     return fetch(event.request)
+    //         .then((response) => {
+    //             return response.text();
+    //         })
+    //         .then((responseBody) => {
+    //             return caches.match('/assets/website/html/layout.html');
+    //         });
+    // };
+
+    // const articleHandler = () => {
+    //    return !online ? new Response(caches.match('/assets/website/html/layout.html')) : ;
+    // };
+
+
     workbox.routing.registerRoute(
         new RegExp('/admin/user/form/.*'),
-        function() {
-            return caches.match('/assets/website/html/layout.html');
+        async ({event}) => {
+            try {
+                return await workbox.strategies.cacheFirst().handle({event});
+            } catch (error) {
+                return caches.match('/assets/website/html/layout.html');
+            }
         }
     );
+
+    workbox.routing.registerRoute(new RegExp('/admin/user/form/.*'), args => {
+        return articleHandler.handle(args);
+    });
+
+
+    workbox.routing.registerRoute(
+        new RegExp('/admin/user/form/.*'),
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'form-cache',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [200, 404],
+                    headers: {
+                        'X-Is-Cacheable': 'true',
+                    },
+                })
+            ]
+        })
+    );
+
 
 
     workbox.routing.registerRoute(
@@ -31,6 +79,7 @@ if (workbox) {
             return online ? false : caches.match('/assets/css/app.min.css');
         }
     );
+
     workbox.routing.registerRoute(
         /.*\/assets\/css\/app-1.min.*/,
         function() {
@@ -58,8 +107,6 @@ if (workbox) {
             return online ? false : caches.match('/assets/fonts/material-design/Material-Design-Iconic-Font.woff2');
         }
     );
-
-
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
