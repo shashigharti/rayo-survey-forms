f2fc41f8d2a65ddf8224ff7595b69db95d80b385
Bhumika <ganeshbhumika876@gmail.com>
2019-03-05 15:01:24 +0545

    register-design

--- packages/robust/core/resources/views/admin/auth/register.blade.php
+++ packages/robust/core/resources/views/admin/auth/register.blade.php
@@ -1,83 +1,112 @@
 @extends('core::admin.layouts.login')
 
 @section('content')
-
-    <div id="signup-box" class="signup-box">
-        <div class="widget-body">
-            <div class="widget-main">
-                <h4 class="header green lighter bigger">
-                    <i class="ace-icon fa fa-users blue"></i>
-                    New User Registration
-                </h4>
-
-                <div class="space-6"></div>
-                <p> Enter your details to begin: </p>
-
-                <form>
-                    <fieldset>
-                        <label class="block clearfix">
-                            <span class="block input-icon input-icon-right">
-                                <input type="email" class="form-control" placeholder="Email"/>
-                                <i class="ace-icon fa fa-envelope"></i>
-                            </span>
-                        </label>
-
-                        <label class="block clearfix">
-                            <span class="block input-icon input-icon-right">
-                                <input type="text" class="form-control"
-                                                                   placeholder="Username"/>
-                                <i class="ace-icon fa fa-user"></i>
-                            </span>
-                        </label>
-
-                        <label class="block clearfix">
-                            <span class="block input-icon input-icon-right">
-                                <input type="password" class="form-control"
-                                                                   placeholder="Password"/>
-                                <i class="ace-icon fa fa-lock"></i>
-                            </span>
-                        </label>
-
-                        <label class="block clearfix">
-                            <span class="block input-icon input-icon-right">
-                                <input type="password" class="form-control"
-                                                                   placeholder="Repeat password"/>
-                                <i class="ace-icon fa fa-retweet"></i>
-                            </span>
-                        </label>
-
-                        <label class="block">
-                            <input type="checkbox" class="ace"/>
-                            <span class="lbl">
-                                I accept the <a href="#">User Agreement</a>
-                            </span>
-                        </label>
-
-                        <div class="space-24"></div>
-
-                        <div class="clearfix">
-                            <button type="reset" class="width-30 pull-left btn btn-sm">
-                                <i class="ace-icon fa fa-refresh"></i>
-                                <span class="bigger-110">Reset</span>
-                            </button>
-
-                            <button type="button"
-                                    class="width-65 pull-right btn btn-sm btn-success">
-                                <span class="bigger-110">Register</span>
-
-                                <i class="ace-icon fa fa-arrow-right icon-on-right"></i>
-                            </button>
-                        </div>
-                    </fieldset>
-                </form>
+<div class="page">
+    <div class="page-content">
+        <div class="page-brand-info">
+            <div class="brand clearfix">
+                <div class="brand-info">
+                    <div class="brand-txt">
+                        <img class="brand-img"
+                             src="{{ (settings('general-setting', 'logo') != '') ? settings('general-setting', 'logo') : url('assets/images/logo-rayo-insight.png') }}"
+                             alt="company logo"
+                             class="img-rounded">
+                        <p>{{ settings('general-setting', 'description') }}</p>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="panel pull-right">
+            <div class="panel-body">
+                <div class="responsive-logo">
+                    <img class="brand-img2"
+                         src="{{ (settings('general-setting', 'logo') != '') ? settings('general-setting', 'logo') : url('assets/images/logo-rayo-insight.png') }}"
+                         alt="company logo"
+                         class="img-rounded">
+                </div>
+                <h3 class="text-center">REGISTER HERE</h3>
 
-            <div class="toolbar center">
-                <a href="#" data-target="#login-box" class="back-to-login-link">
-                    <i class="ace-icon fa fa-arrow-left"></i>
-                    Back to login
-                </a>
+                {{ Form::open(['route' => 'auth.check', 'role' => 'form', 'class' => 'form', 'autocomplete'=> 'off']) }}
+                <div class="form-group form-material floating {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label class="floating-label">Full Name</label>
+                    {{ Form::email('email', null, [
+                     'class'       => 'form-control',
+                     'required'    => 'required',
+                 ]) }}
+                    
+                </div>
+                <div class="form-group form-material floating {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label class="floating-label">Email</label>
+                    {{ Form::email('email', null, [
+                     'class'       => 'form-control',
+                     'required'    => 'required',
+                 ]) }}
+
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                    @endif
+                </div>               
+                <div class="form-group form-material floating">
+                    <label class="floating-label">New Password</label>
+
+                    <div class="{{ $errors->has('password') ? 'has-error' : '' }} control-required">
+                        {{ Form::password('password', [
+                            'class'       => 'form-control',
+                            'required'    => 'required'
+                        ]) }}
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group form-material floating">
+                    <label class="floating-label">Confirm Password</label>
+
+                    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }} control-required">
+                        {{ Form::password('password', [
+                            'class'       => 'form-control',
+                            'required'    => 'required'
+                        ]) }}
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
+                    </div>
+                </div>
+             
+                <button type="submit" class="btn btn-primary margin-top-40 sign-in btn-block btn-lg">Register
+                </button>
+                {{ Form::close() }}
+
+               <p>Already Registered? Please go to <a href="#">Log in</a></p>               
+
             </div>
-        </div><!-- /.widget-body -->
-    </div><!-- /.signup-box -->
+           <footer class="page-copyright text-center">
+                <div class="social-share">
+                    <a href="https://www.facebook.com/robustitconcepts" target="_blank"><img class="social-icon"
+                                                                                             src="{{ URL::asset('assets/images/fb.png') }}"
+                                                                                             alt="facebook"
+                                                                                             class="img-rounded"></a>
+                    <a href="https://twitter.com/robustITconcept" target="_blank"><img class="social-icon"
+                                                                                       src="{{ URL::asset('assets/images/tw.png') }}"
+                                                                                       alt="facebook"
+                                                                                       class="img-rounded"></a>
+                    <a href="https://www.linkedin.com/company/robust-it-concepts-pvt-ltd" target="_blank"><img
+                                class="social-icon" src="{{ URL::asset('assets/images/in.png') }}" alt="facebook"
+                                class="img-rounded"></a>
+                </div>
+                <p>Robust IT Concepts</p>
+                <p>© 2019. All RIGHT RESERVED.</p>
+
+            </footer> 
+        </div>
+
+    </div>
+</div>
+<!-- End Page -->
 @endsection
