dc89e2a4c9a7c6d3a51b83b38a10fe810cb29720
Zenish Shrestha <zenishshr@gmail.com>
2019-03-05 12:54:33 +0545

    Registration demo page'

--- packages/robust/core/routes/admin/auth/auth.php
+++ packages/robust/core/routes/admin/auth/auth.php
@@ -5,6 +5,10 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.', 'group' => 'Auth'], function 
         'as' => 'login',
         'uses' => '\Robust\Core\Controllers\Auth\LoginController@getLogin'
     ]);
+    Route::get('/register', [
+        'as' => 'register',
+        'uses' => '\Robust\Core\Controllers\Auth\LoginController@getRegister'
+    ]);
     Route::post('/check', [
         'as' => 'check',
         'uses' => '\Robust\Core\Controllers\Auth\LoginController@login'
