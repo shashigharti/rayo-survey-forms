734b76ef2c5b4430325942771001422c7f69fd8e
Zenish Shrestha <zenishshr@gmail.com>
2019-03-06 13:32:41 +0545

    Revert "Landing page"
    This reverts commit 6d4d6432ee85ab5c4f3ffa6219c64431c702133a.

--- packages/robust/core/public/website/js/app.js
+++ packages/robust/core/public/website/js/app.js
@@ -1,7 +1,3 @@
 //import("../../../../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js");
 //import("modules/modal.js")
 //import("modules/social.js")
-
-//import("modules/main.js")
-//import("modules/flexslider.js")
-//import("modules/site.js")
--- packages/robust/core/routes/website/website.php
+++ packages/robust/core/routes/website/website.php
@@ -4,7 +4,3 @@ Route::get('/', [
     'uses' => '\Robust\Core\Controllers\Website\HomeController@index'
 ]);
 
-Route::get('/home', [
-    'as' => 'website.home',
-    'uses' => '\Robust\Core\Controllers\Website\HomeController@index'
-]);
