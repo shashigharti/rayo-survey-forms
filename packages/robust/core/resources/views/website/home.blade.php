01ddb32fe8a781a0f75d0c4dc8b86db443b21bbd
Zenish Shrestha <zenishshr@gmail.com>
2019-03-06 13:32:41 +0545

    Revert "Landing page"
    This reverts commit 6d4d6432ee85ab5c4f3ffa6219c64431c702133a.

--- packages/robust/core/resources/views/website/home.blade.php
+++ packages/robust/core/resources/views/website/home.blade.php
@@ -1,22 +1,3 @@
-@extends(Site::templateResolver('core::website.layouts.default'))
-@section('content')
-    @if(\Auth::check())
-
-
-        <div class="container dashboard-container">
-            @include(Site::templateResolver('core::website.partials.common.header'))
-            <div class="menu-dashboard">
-                @include(Site::templateResolver('core::website.partials.common.left-menu'))
-            </div>
-            <div class="page dashboard-content">
-            </div>
-            @include(Site::templateResolver('core::website.partials.common.footer'))
-        </div>
-    @else
-        @include(Site::templateResolver('core::website.partials.common.sub-header'))
-        @include(Site::templateResolver('core::website.partials.dashboard.main-content'))
-        @include(Site::templateResolver('core::website.partials.dashboard.footer'))
-    @endif
-@endsection
-
-
+<script type="text/javascript">
+    window.location = "{{ url('/admin') }}";//here double curly bracket
+</script>
