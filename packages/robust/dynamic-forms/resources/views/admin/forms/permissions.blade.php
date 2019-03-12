9fc50034f75c71b4fa2b707fc5a16a148c985dec
Zenish Shrestha <zenishshr@gmail.com>
2019-03-12 16:22:59 +0545

    Permissions feature init

--- packages/robust/dynamic-forms/resources/views/admin/forms/permissions.blade.php
+++ packages/robust/dynamic-forms/resources/views/admin/forms/permissions.blade.php
@@ -2,21 +2,38 @@
 
 @section('form')
     @set('ui', new $ui)
-
+    <form action="#" method="GET">
     <div id="form_permission">
         <input type="hidden" name="base_url" value="{{url('/')}}">
-        <fieldset>
-            <legend>Roles:</legend>
-            <v-select tabindex="1" v-model="selected_roles" :options="available_roles"
-                      placeholder="Comma Separated Value For Options E.g, Manager, Accountant" multiple></v-select>
-            <input type="hidden" name="roles"  value="{{json_encode($model->roles()->pluck('id'))}}">
-        </fieldset>
-        <fieldset>
-            <legend>Users:</legend>
-            <v-select tabindex="2" v-model="selected_users" :options="available_users"
-                      placeholder='Comma Separated Value For Options E.g, Rita, Michelle' multiple></v-select>
-            <input type="hidden" name="users" value="{{json_encode($model->users()->pluck('id'))}}">
-            <input type="hidden" name="form_id" value="{{$model->id}}">
-        </fieldset>
+        {{--<fieldset>--}}
+            {{--<legend>Roles:</legend>--}}
+            {{--<select class="form-control js-example-basic-multiple" name="states[]" multiple="multiple">--}}
+                {{--<option value="AL">Alabama</option>--}}
+                {{--<option value="WY">Wyoming</option>--}}
+            {{--</select>--}}
+        {{--</fieldset>--}}
     </div>
+    <div class="form-group">
+        <label for="users">Users</label>
+        <select class="form-control js-example-basic-multiple" id="users" name="users[]" multiple="multiple">
+            @foreach($all_users as $user)
+                <option value="{{$user->id}}">{{$user->first_name . ' ' . $user->last_name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary">Save Permissions</button>
+    </div>
+    </form>
+
+@endsection
+
+@section('js')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-multiple').select2();
+        });
+    </script>
 @endsection
