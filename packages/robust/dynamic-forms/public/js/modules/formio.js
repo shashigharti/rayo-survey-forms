161badba38d75ad35589173deea7f6deb7c8497a
Zenish Shrestha <zenishshr@gmail.com>
2019-03-19 11:05:48 +0545

    Test

--- packages/robust/dynamic-forms/public/js/modules/formio.js
+++ packages/robust/dynamic-forms/public/js/modules/formio.js
@@ -98,7 +98,7 @@
     $(function () {
         let base_url = $('.design--form').data("base-url");
         FRW.DynamicForms.FormIO.init();
-        Formio.setProjectUrl('https://examples.form.io/');
-        // Formio.setProjectUrl('http://127.0.0.1:8000');
+        // Formio.setProjectUrl('https://examples.form.io/');
+        Formio.setProjectUrl('http://127.0.0.1:8000');
     });
 }(jQuery, FRW, window, document));
