f66259cad56c063a0b905cb848d9a307bbb4f6eb
Bisesh Heyojoo <hbisesh19@gmail.com>
2019-12-30 13:52:06 +0545

    delete function solved

---
+++ packages/robust/dynamic-forms/database/migrations/2019_12_30_080604_alter_table_dynform_form_user_table_change_foriegnkey.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableDynformFormUserTableChangeForiegnkey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dynform_form_user', function(Blueprint $table)
+        {
+            $table->dropForeign('dynform_form_user_form_id_foreign');
+            $table->dropForeign('dynform_form_user_user_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dynform_form_user', function(Blueprint $table)
+        {
+            $table->foreign('form_id')->references('id')->on('dynform_forms');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+}
